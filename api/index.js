fe755cad39a57b1b20c0fce5818e4a66e0e74761
arslanhussain9 <arslansageuniversity@gmail.com>
2026-04-26 23:48:41 +0530

    Move static files to public folder for Vercel

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use('/api/auth', authRoutes);
 // Local development
 if (process.env.NODE_ENV !== 'production') {
     const path = require('path');
-    app.use(express.static(path.join(__dirname, '..')));
+    app.use(express.static(path.join(__dirname, '../public')));
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
